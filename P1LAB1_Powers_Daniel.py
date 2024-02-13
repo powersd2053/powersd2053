c3c5dd5e3b2c4310f5786965f2834af52e778d25
powersd2053 <159938091+powersd2053@users.noreply.github.com>
2024-02-13 12:25:06 -0500

    Add files via upload

---
+++ P1LAB1_Powers_Daniel.py
@@ -0,0 +1,10 @@
+#Daniel Powers
+#2/12/24
+#P1LAB1
+#Using input and output
+
+#Create a variable to hold first name
+first_name = input("Enter your first name: ")
+
+#Print first_name with greeting
+print("Hello", first_name, "\nWelcome to zyBooks!")
