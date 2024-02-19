04d5764249fa0931d5a1cee5beb7f41070ca1e77
powersd2053 <159938091+powersd2053@users.noreply.github.com>
2024-02-19 16:32:17 +0000

    First HW2 upload

---
+++ HW2_Example.py
@@ -0,0 +1,20 @@
+#Name
+#Date
+#Input and Output
+
+'''Pseudocode:
+Get budget (int)
+Get destination (str)
+Get gas (int)
+Get hotel (int)
+Get food (int)
+Add expenses 
+budget - expenses
+Display results - use example in Module 3 HW2
+
+'''
+
+print ("This programs calculates and displays trael expenses)")
+#Get budget
+budget = int(input("Enter Budget: "))
+destination = str("Nashville")
