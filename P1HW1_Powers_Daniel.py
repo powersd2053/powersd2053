fd1b468cdf3229b01b0baf9b92001d4994b80440
powersd2053 <159938091+powersd2053@users.noreply.github.com>
2024-02-19 10:32:56 -0500

    Add files via upload

---
+++ P1HW1_Powers_Daniel.py
@@ -0,0 +1,45 @@
+#Daniel Powers
+#02/14/24
+#P1HW1
+#Basic output with variables
+
+#Define integer1
+integer1 = int(input("Enter integer:"))
+
+#define squared
+
+squared = integer1 **2
+
+#define cubed
+
+cubed = integer1 **3
+
+
+
+
+#PRINT Statements
+
+print ("You entered:", integer1)
+
+print (integer1, "squared is", squared)
+
+print (integer1, "cubed is", cubed, " !!")
+
+#define integer2
+
+integer2 = int(input("Enter another integer:"))
+
+#define sum
+
+Sum = integer1 + integer2
+
+#define product
+
+Product = integer1 * integer2
+
+print(integer1, "+", integer2, "is", Sum)
+
+print(integer1, "*", integer2, "is", Product)
+
+
+
